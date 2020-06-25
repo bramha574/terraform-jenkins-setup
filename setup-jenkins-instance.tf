82ab123d123b274cbe0489487da8a782c4c0a23e
bramha574 <veeranaidu2@gmail.com>
2020-06-25 16:50:43 -0500

    change public ip to a message

--- setup-jenkins-instance.tf
+++ setup-jenkins-instance.tf
@@ -37,14 +37,14 @@ resource "aws_security_group" "jenkins_server_sgs" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0", "10.0.0.14/32"]
+    cidr_blocks = ["0.0.0.0/0", "<desired public ip>/32"]
   }
 
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0", "10.0.0.14/32"]
+    cidr_blocks = ["0.0.0.0/0", "<desired public ip>/32"]
   }
 
   ingress {
